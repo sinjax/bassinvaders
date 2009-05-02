554101156b500114a3899552c8d205a21258dacf
Darren Golbourn <golbourn@gmail.com>
2009-05-02 18:39:34 +0100

    actually included missing files :)

---
+++ BassInvaders/BandPassFilterFFT.cpp
@@ -0,0 +1,113 @@
+/*
+ * BandPassFilterFFT.cpp
+ *
+ *  Created on: May 2, 2009
+ *      Author: Darren Golbourn
+ */
+
+#include "BandPassFilterFFT.h"
+
+/*
+ * Initialize the FFT Band-pass filter
+ */
+BandPassFilterFFT::BandPassFilterFFT(uint32_t sample_rate, uint32_t chunk_size){
+	length = chunk_size;
+	samples = length/4;
+	freqs = samples/2;
+
+	/* allocate fast Fourier transform stuff */
+	workspace = gsl_fft_complex_workspace_alloc (samples);
+	wavetable = gsl_fft_complex_wavetable_alloc (samples);
+
+	/* frequency array */
+	double delta = 1./(sample_rate-1);
+	f = new double[freqs+1];
+	f[0] = 0;
+	for (uint32_t i=1; i<=freqs; i++){
+		f[i]=i/(samples*delta);
+	}
+
+	//faccel = gsl_interp_accel_alloc () ;
+	fcache = new double[2*samples];
+}
+
+/**
+ * find the nearest index i to frequency freq in fft such that
+ * f[i] < freq <f[i+1]
+ */
+int BandPassFilterFFT::ffind(double freq){
+	return gsl_interp_bsearch(f, freq, 1, freqs);//gsl_interp_accel_find (faccel, f, freqs, freq );
+}
+
+/*
+ * transform stream in to frequency domain
+ */
+fft_t BandPassFilterFFT::fft_alloc(uint8_t* stream)
+{
+	for (uint32_t i =0; i<samples; i++){
+		REAL(fcache,i)=(double)RIGHT((int16_t*)stream,i);
+		IMAG(fcache,i)=(double)LEFT((int16_t*)stream,i);
+	}
+
+	/* Fourier transform */
+	gsl_fft_complex_forward(fcache, 1, samples, wavetable, workspace);
+
+	return fcache;
+}
+
+/*
+ * Free the allocated data types
+ */
+BandPassFilterFFT::~BandPassFilterFFT() {
+	delete[] f;
+	delete[] fcache;
+	//gsl_interp_accel_free(faccel);
+	gsl_fft_complex_wavetable_free (wavetable);
+	gsl_fft_complex_workspace_free (workspace);
+}
+
+void BandPassFilterFFT::ingest(uint8_t *stream)
+{
+	fft_alloc(stream);
+}
+
+void BandPassFilterFFT::band_pass(uint8_t *stream, double flo, double fhi)
+{
+	band_window(fcache, stream, ffind(fhi)+1, ffind(flo));
+}
+
+/*
+ * using the frequency fft data, band pass between frequencies in f[bandlo] <= f <= f[bandhi]
+ */
+void BandPassFilterFFT::band_window(fft_t data, uint8_t * stream, uint32_t bandhi, uint32_t bandlo)
+{
+	/*
+	 * Split the data in to frequency bands.
+	 */
+	double band_data[2*samples];
+
+	for(uint32_t i=0; i<=freqs; i++) // Go over each frequency...
+	{
+		if ((i <= bandhi) && (i>=bandlo)) // ...and copy over the frequencies in the the band window...
+		{
+			REAL(band_data,POSITIVE(i,samples)) = REAL(data,POSITIVE(i,samples));
+			IMAG(band_data,POSITIVE(i,samples)) = IMAG(data,POSITIVE(i,samples));
+			REAL(band_data,NEGATIVE(i,samples)) = REAL(data,NEGATIVE(i,samples));
+			IMAG(band_data,NEGATIVE(i,samples)) = IMAG(data,NEGATIVE(i,samples));
+		}
+		else // ...and set the other frequencies to zero.
+		{
+			REAL(band_data,POSITIVE(i,samples)) = 0.;
+			IMAG(band_data,POSITIVE(i,samples)) = 0.;
+			REAL(band_data,NEGATIVE(i,samples)) = 0.;
+			IMAG(band_data,NEGATIVE(i,samples)) = 0.;
+		}
+	}
+
+	gsl_fft_complex_inverse(band_data, 1, samples, wavetable, workspace); // Then reverse Fourier transform back to the time domain...
+
+	for (uint32_t i =0; i<samples; i++){ // ...and copy the data back in to the stream.
+		RIGHT((int16_t*)stream,i)=(int16_t)REAL(band_data,i);
+		LEFT((int16_t*)stream,i)=(int16_t)IMAG(band_data,i);
+	}
+}
---
+++ BassInvaders/BandPassFilterFFT.h
@@ -0,0 +1,42 @@
+/*
+ * BandPassFilterFFT.h
+ *
+ *  Created on: May 2, 2009
+ *      Author: Darren Golbourn
+ *
+ * Description: This library lets you split an audio stream in to frequency bands.
+ */
+
+#ifndef BANDPASSFILTERFFT_H_
+#define BANDPASSFILTERFFT_H_
+
+#include <inttypes.h>
+#include "gsl/gsl_errno.h"
+#include "gsl/gsl_fft_complex.h"
+#include "gsl/gsl_interp.h"
+#include "audefnmacros.h"
+
+typedef double* fft_t;
+
+class BandPassFilterFFT {
+	/* wavetable and workspace are used by the fft algorithm */
+	gsl_fft_complex_wavetable *wavetable;
+	gsl_fft_complex_workspace *workspace;
+	uint32_t length; 			// length of the sound chunk in bytes
+	uint32_t bands; 			// number of bands to partition the stream in to
+	uint32_t samples;			// number of 16bit-per-channel stereo samples
+	uint32_t freqs; 			// number of positive frequencies
+	double *f; 					// array of frequencies
+	//gsl_interp_accel * faccel; 	// accelerator to help searching for indices of f.
+	fft_t fcache;				// cache of frequency domain data
+	int ffind(double);			// search function for frequency data
+	double* fft_alloc(uint8_t* stream); // allocate the fourier transform
+	void band_window(fft_t data, uint8_t * stream, uint32_t bandhi, uint32_t bandlo); // the band pass filter
+public:
+	BandPassFilterFFT(uint32_t, uint32_t);
+	virtual ~BandPassFilterFFT();
+	void ingest(uint8_t *stream); // eat a stream and fourier-transform it.
+	void band_pass(uint8_t *stream, double flo, double fhi); // copy band passed version ingested data to stream.
+};
+
+#endif /* BANDPASSFILTERFFT_H_ */
