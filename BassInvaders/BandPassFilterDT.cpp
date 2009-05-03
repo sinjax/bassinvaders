920e801c7e2879b2431613e0ff13b377fb9fcd97
Darren Golbourn <golbourn@gmail.com>
2009-05-03 20:30:09 +0100

    added DT band pass high pass filter

--- BassInvaders/BandPassFilterDT.cpp
+++ BassInvaders/BandPassFilterDT.cpp
@@ -9,8 +9,8 @@
 
 BandPassFilterDT::BandPassFilterDT(uint32_t chunk_size) {
 	samples = chunk_size/4;
-	L = new history<int16_t>(1);
-	R = new history<int16_t>(1);
+	L = new history<int16_t>(2);
+	R = new history<int16_t>(2);
 }
 
 BandPassFilterDT::~BandPassFilterDT() {
@@ -33,12 +33,29 @@ void BandPassFilterDT::low_pass(uint8_t *stream, double alpha)
 	}
 
 	L->record(LEFT(S,i));
-	R->record(LEFT(S,i));
+	R->record(RIGHT(S,i));
 }
-
-
 
 void BandPassFilterDT::high_pass(uint8_t *stream, double alpha)
 {
+	int16_t* S = (int16_t*)stream;
+	uint32_t i = 0;
 
+	LEFT(S,i) = alpha * ( LEFT(S,i) - L->current() - L->current(-1));
+	RIGHT(S,i) = alpha * ( RIGHT(S,i) - R->current() - L->current(-1));
+	int16_t XL = L->current();
+	int16_t XR = R->current();
+
+	for(i=1; i<samples; i++)
+	{
+		LEFT(S,i) = alpha * ( LEFT(S,i) - LEFT(S,i-1) - XL);
+		RIGHT(S,i) = alpha * ( RIGHT(S,i) - RIGHT(S,i-1) - XR);
+		XL = LEFT(S,i-1);
+		XR = RIGHT(S,i-1);
+	}
+
+	L->record(LEFT(S,i-1));
+	L->record(LEFT(S,i));
+	R->record(RIGHT(S,i-1));
+	R->record(RIGHT(S,i));
 }
